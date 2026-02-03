755e0017c150a8c7911be3313e8870b0495ff8fb
jordansds <56271228+jordansds@users.noreply.github.com>
2026-02-03 22:06:28 +0000

    Delete worlds/tww3 directory
    Removed so I can upload new files.

--- worlds/tww3/item_tables/faction_item_table.py
+++
@@ -1,8 +0,0 @@
-from BaseClasses import ItemClassification as IC
-from typing import Dict
-
-from .item_types import ItemType, ItemData
-
-factionItemDict: Dict[int, ItemData] = {
-    4000: ItemData(IC.progression, 88, "wh2_dlc09_ritual_crafting_tmb_army_capacity_25", ItemType.effect_faction, "None", None, "None")
-}
--- worlds/tww3/item_tables/progression_table.py
+++
@@ -1,8 +0,0 @@
-from .item_types import ItemType, ItemData
-from BaseClasses import ItemClassification as IC
-
-progressionDict: dict[int, ItemData] = {
-   1000: ItemData(IC.progression, 0, "Administrative Capacity", ItemType.progression, "None", None, None),
-   1001: ItemData(IC.progression, 0, "Diplomatic Range", ItemType.progression, "None", None, None),
-   1002: ItemData(IC.progression, 0, "Orb of Domination", ItemType.goal, "None", None, None)
-}
