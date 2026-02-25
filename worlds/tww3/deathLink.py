4a913b37c3bdfa8e0ae8ef0950fe137b9c5d5f27
jordansds <jordansds3@gmail.com>
2026-02-25 22:30:00 +0000

    Working on deathlink

--- worlds/tww3/deathLink.py
+++ worlds/tww3/deathLink.py
@@ -1,28 +1,19 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from .TWW3Client import TWW3Context
     from .world import TWW3World
 
-deathLinkFunctions: list[str] = ["", "", ""]
+#deathLinkFunctions: list[str] = ["remove_treasury_percentage(10)", "remove_treasury_percentage(25)", "remove_treasury_percentage(50)"]
+deathLinkFunctionDict: dict[str, str] = {
+    "10% Treasury" : "remove_treasury_percentage(10)",
+    "25% Treasury": "remove_treasury_percentage(25)",
+    "50% Treasury": "remove_treasury_percentage(50)",
+}
 #deathLinkOptions: list[str] = []
 
 #Pull the enabled deathlink functions chosen in the yaml
 def createDeathLinkFunctions(world: TWW3World):
-    deathLinkOptions: list[bool] = [*world.options.death_link_effect]
-    return [func for index, func in enumerate(deathLinkFunctions) if deathLinkOptions[index]]
-
-async def receiveDeathLink(ctx: 'CivVIContext', message: str):
-    deathLinkOptions = ctx.slot_data["death_link_options"]
-
-async def checkDeathLink(ctx: 'CivVIContext', message: str):
-    if ctx.received_death_link:
-        ctx.received_death_link = False
-        await receiveDeathLink(ctx, ctx.death_link_message)
-
-    result = await ctx.game_interface.get_deathlink()
-    if ctx.death_link_just_changed:
-        ctx.death_link_just_changed = False
-        return
-
+    return [deathLinkFunctionDict[key] for key in world.options.death_link_effect]
+    #deathLinkOptions: list[bool] = [*world.options.death_link_effect]
+    #return [func for key, func in enumerate(deathLinkFunctions) if deathLinkOptions[index]]
