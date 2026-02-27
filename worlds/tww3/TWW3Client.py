d5a0e86534d204bb61c673a9997db0ef0da5b3e8
jordansds <jordansds3@gmail.com>
2026-02-27 22:22:24 +0000

    Deathlink

--- worlds/tww3/TWW3Client.py
+++ worlds/tww3/TWW3Client.py
@@ -14,6 +14,7 @@ from .item_tables.ritual_table import ritualDict
 from .item_tables.progression_table import progressionDict
 from . import TWW3World
 from . import settlementManager as sm
+from . import deathLink
 import os
 from NetUtils import ClientStatus
 
@@ -156,9 +157,12 @@ class TWW3Context(CommonContext):
 
         self.deathLinkEnabled = args['slot_data']["death_link"]
         if self.deathLinkEnabled:
-        #if "death_link" in self.hades_slot_data and self.hades_slot_data["death_link"]:
             asyncio.create_task(self.update_death_link(True))
             logger.info("DeathLink is enabled, good luck...")
+
+        self.deathLinkEffects = args['slot_data']["death_link_effects"]
+
+        self.deathLinkOptions = deathLink.createDeathLinkFunctions(self.deathLinkEffects)
 
         self.watcher = Watcher(os.path.join(self.path, "engine.out"), self)
         watcher_task = asyncio.create_task(self.watcher.watch(self.gameMode), name='watcher')
