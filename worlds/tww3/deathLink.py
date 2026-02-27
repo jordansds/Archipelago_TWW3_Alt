f5a3307bb33f67ce9dc735dbb5b5ad309c3f825c
jordansds <jordansds3@gmail.com>
2026-02-27 23:08:22 +0000

    Deathlink

--- worlds/tww3/deathLink.py
+++ worlds/tww3/deathLink.py
@@ -1,10 +1,6 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from .world import TWW3World
-
-#deathLinkFunctions: list[str] = ["remove_treasury_percentage(10)", "remove_treasury_percentage(25)", "remove_treasury_percentage(50)"]
 deathLinkFunctionDict: dict[str, str] = {
     "10% Treasury" : "remove_treasury_percentage(10)",
     "25% Treasury": "remove_treasury_percentage(25)",
@@ -12,12 +8,10 @@ deathLinkFunctionDict: dict[str, str] = {
     "Wound Hero": "wound_random_hero()",
     "Wound Lord": "wound_random_lord()",
     "Rebellion": "force_random_strong_rebellion_for_player()",
-    "Raze Random Settlement": "raze_random_settlement()"
+    "Raze Random Settlement": "raze_random_settlement()",
+    "Disable Replenishment (2 turns)": "deathlink_disable_replenishment()"
 }
-#deathLinkOptions: list[str] = []
 
 #Pull the enabled deathlink functions chosen in the yaml
 def createDeathLinkFunctions(deathLinkEffects):
     return [deathLinkFunctionDict[key] for key in deathLinkEffects]
-    #deathLinkOptions: list[bool] = [*world.options.death_link_effect]
-    #return [func for key, func in enumerate(deathLinkFunctions) if deathLinkOptions[index]]
