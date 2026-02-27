e4c26dc1a266c3e3855a9d41761dce3688984d67
Jordansds <jordansds3@gmail.com>
2026-02-27 11:31:10 +0000

    Fixed bug with Linux filesystems

--- worlds/tww3/TWW3Client.py
+++ worlds/tww3/TWW3Client.py
@@ -135,18 +135,18 @@ class TWW3Context(CommonContext):
         self.path = TWW3World.settings.tww3_path
         
         if not self.path or not os.path.exists(self.path):
-            logger.error('ERROR: Could not find Warhammer folder. Please remove path from host.yaml.')
+            logger.error('ERROR: Could not find Warhammer folder. Please correct the path in your host.yaml.')
             Utils.async_start(self.disconnect())
-        if not os.path.isfile(self.path + '\\Warhammer3.exe'):
-            logger.error('ERROR: Could not find Warhammer3.exe. Please remove path from host.yaml.')
+        if not os.path.isfile(os.path.join(self.path, "Warhammer3.exe")):
+            logger.error('ERROR: Could not find Warhammer3.exe. Please correct the path in your host.yaml.')
             Utils.async_start(self.disconnect())
 
         self.gameMode = args['slot_data']['game_mode']
         logger.info(f"The game mode is: {self.gameMode}")
             
-        self.watcher = Watcher(self.path + '\\engine.out', self)
+        self.watcher = Watcher(os.path.join(self.path, "engine.out"), self)
         watcher_task = asyncio.create_task(self.watcher.watch(self.gameMode), name='watcher')
-        self.messenger = Messenger(self.path + '\\engine.in')
+        self.messenger = Messenger(os.path.join(self.path, "engine.in"))
 
         self.playerFaction = sm.factionDict[args["slot_data"]["starting_faction"]].name
 
