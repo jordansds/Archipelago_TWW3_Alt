34805d6ad6eacc53cf4c255864b9ee520d5658aa
Jordansds <jordansds3@gmail.com>
2026-03-10 12:28:49 +0000

    Added empty tables

---
+++ worlds/tww3/mod_tables/southernRealms.py
@@ -0,0 +1,30 @@
+from BaseClasses import ItemClassification as IC
+from worlds.tww3.item_types import ItemType, ItemData, specialItemData
+
+units: dict[int, ItemData] = {
+
+}
+
+buildings: dict[int, ItemData] = {
+
+}
+
+techs: dict[int, ItemData] = {
+
+}
+
+progUnits: dict[int, ItemData] = {
+
+}
+
+progBuildings: dict[int, ItemData] = {
+
+}
+
+progTechs: dict[int, ItemData] = {
+
+}
+
+special: dict[int, specialItemData] = {
+
+}
