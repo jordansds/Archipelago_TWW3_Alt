3ec0a6ec4a1796e905364f96372c397b49cdcfd1
jordansds <56271228+jordansds@users.noreply.github.com>
2026-05-07 00:56:24 +0100

    Update TWW3Client.py
    Fixed spheres crashing when collecting diplomatic range item

--- worlds/tww3/TWW3Client.py
+++ worlds/tww3/TWW3Client.py
@@ -334,6 +334,7 @@ class TWW3Context(CommonContext):
             self.orbGoal = args['slot_data']['orbs']
             self.spheres = args['slot_data']['spheres']
             self.numberOfOrbs = 0
+            self.expansionItems = 0
 
             offset = sum([1 for i in range(1, len(sm.settlementDict) + 1) for j in range(10)]) + 1
             for key, settlement in sm.settlementDict.items():
