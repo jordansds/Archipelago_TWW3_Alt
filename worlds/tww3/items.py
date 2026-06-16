ec7db1aa6afdd4a60ebb27f210616f5e56bce5c8
jordan <jordansds3@gmail.com>
2026-06-16 17:55:34 +0100

    Made items more likely for filler

--- worlds/tww3/items.py
+++ worlds/tww3/items.py
@@ -216,6 +216,10 @@ def generateFillerItems(world: TWW3World, pool: list) -> list:
 
 def generateFiller(world: TWW3World) -> TWW3Item:
     key = world.random.choice(tuple(fillerDict.keys()))
+    #Make Items twice as likely to get :) They're fun
+    if key != 1205:
+        key = world.random.choice(tuple(fillerDict.keys()))
+
     if key == 1207 and world.options.game_mode == "conquest":
         key = 1205
     if key == 1205:
