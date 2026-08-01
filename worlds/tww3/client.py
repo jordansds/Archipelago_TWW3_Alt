fac092a4b7d3b9f0d0af60d18232eb870ca90d48
jordan <jordansds3@gmail.com>
2026-08-01 15:18:38 +0100

    Let users set their paths

--- worlds/tww3/client.py
+++ worlds/tww3/client.py
@@ -324,7 +324,7 @@ class TWW3Context(CommonContext):
             logger.info(f"You are running: {self.version} of the TWW3 APWorld")
 
         self.path = TWW3World.settings.tww3_path
-        #self.path = "/home/jordan/Documents/"
+        self.path = "/home/jordan/Documents/"
         self.seed = args['slot_data']['seed']
 
         #if not self.path or not os.path.exists(self.path):
